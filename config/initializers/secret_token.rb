1e8eca4c8e48d9b6d862d2a82f6e4332139856db
Harris Effron <harris.effron@gmail.com>
2013-07-17 09:11:57 -0400

    Initial commit. Names project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddly::Application.config.secret_token = '75950a8ba88a0c420d1eae992abf28dbf078c2a73de5b2f0a29cc1a256bcd4765804c73e4c66289a219bb3822a9c22b5b3e12a431b91eefc06709786049b7480'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Reddly::Application.config.session_store :cookie_store, key: '_Reddly_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Reddly::Application.config.session_store :active_record_store
